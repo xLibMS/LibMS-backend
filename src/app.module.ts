b5e539c833b3fbdf64aa4352057ded7d69a01d6a
Marwen Dallel <71770363+MarwenDallel@users.noreply.github.com>
2021-04-09 10:46:51 +0100

    build(deps): remove vulnerable package nest-event

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from 'src/modules/user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { NestEventModule } from 'nest-event';
 import { UserRepository } from '@modules/user/database/user.repository';
 import { UserOrmEntity } from '@modules/user/database/user.orm-entity';
 import { typeormConfig } from './infrastructure/configs/ormconfig';
@@ -10,7 +9,6 @@ import { typeormConfig } from './infrastructure/configs/ormconfig';
   imports: [
     TypeOrmModule.forRoot(typeormConfig),
     TypeOrmModule.forFeature([UserOrmEntity]),
-    NestEventModule,
     UserModule,
   ],
   controllers: [],
