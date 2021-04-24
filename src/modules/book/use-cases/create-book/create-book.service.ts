6a3b63fef06d25593e579189cc6f830a8a679775
Mootez <mootez.saad@medtech.tn>
2021-04-24 17:27:18 +0100

    feat: update add book application service for image upload

--- src/modules/book/use-cases/create-book/create-book.service.ts
+++ src/modules/book/use-cases/create-book/create-book.service.ts
@@ -1,11 +1,15 @@
 import { BookRepositoryPort } from '@modules/book/database/book.repository.interface';
+import { FileUplaodService } from '@modules/book/domain-services/file-upload.service';
 import { BookEntity } from '@modules/book/domain/entities/book.entity';
 import { ConflictException } from '@nestjs/common';
 import { ID } from 'src/core/value-objects/id.value-object';
 import { CreateBookCommand } from './create-book.command';
 
 export class CreateBookService {
-  constructor(private readonly bookRepo: BookRepositoryPort) {}
+  constructor(
+    private readonly bookRepo: BookRepositoryPort,
+    private readonly imageService: FileUplaodService,
+  ) {}
 
   async createBook(command: CreateBookCommand): Promise<ID> {
     if (await this.bookRepo.existsByISBN(command.isbn.isbn13)) {
@@ -17,6 +21,8 @@ export class CreateBookService {
       }
     }
     const book = new BookEntity(command);
+
+    this.imageService.upload(command.storedImage, command.image);
 
     const created = await this.bookRepo.save(book);
 
