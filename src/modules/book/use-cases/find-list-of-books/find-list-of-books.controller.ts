855d9cea2ce025da6310b0b030f9844aea964f94
MohamedBechir <jamoussi.bechir@yahoo.com>
2021-05-04 02:25:34 +0200

    feat: add guard for retrieving list of books

--- src/modules/book/use-cases/find-list-of-books/find-list-of-books.controller.ts
+++ src/modules/book/use-cases/find-list-of-books/find-list-of-books.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { routes } from '@config/app.routes';
 import { ListOfBooksResponse } from '@modules/book/dtos/list-of-books.response.dto';
 import { BookRepository } from '@modules/book/database/book.repository';
+import { JwtAuthGuard } from '@modules/user/guards/jwt-auth.guard';
 
 @Controller()
 export class FindListOfBooksHttpController {
   constructor(private readonly bookRepo: BookRepository) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get(routes.book.list)
   async findByEmail(): Promise<ListOfBooksResponse> {
     const user = await this.bookRepo.findAllBooks();
