2031e22b1440a4435666af865fc1d4d12ac6dea5
Marwen Dallel <71770363+MarwenDallel@users.noreply.github.com>
2021-05-06 11:00:47 +0100

    refactor: fix user factory to respect domain rules

--- src/modules/user/database/seeding/user.factory.ts
+++ src/modules/user/database/seeding/user.factory.ts
@@ -1,13 +1,22 @@
 import { define } from 'typeorm-seeding';
 import Faker from 'faker';
+import { emailDomainsWhitelist } from '@config/mailer.config';
 import { UserOrmEntity } from '../user.orm-entity';
 
 define(UserOrmEntity, (faker: typeof Faker) => {
+  const emailProvider =
+    emailDomainsWhitelist[
+      Math.floor(Math.random() * emailDomainsWhitelist.length)
+    ];
   const user = new UserOrmEntity();
   user.id = faker.random.uuid();
-  user.email = faker.internet.email();
   user.firstName = faker.name.firstName();
   user.lastName = faker.name.lastName();
+  user.email = faker.internet.email(
+    user.firstName.toLocaleLowerCase(),
+    user.lastName.toLocaleLowerCase(),
+    emailProvider,
+  );
   user.universityID = faker.random
     .number({ min: 1000000, max: 9999999 })
     .toString();
