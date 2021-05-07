f7611581b46a207145311f67b0386c4a24d74e09
Mootez Saad <mootez.saad@medtech.tn>
2021-05-07 11:51:48 +0100

    refactor: configure server to serve static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from 'src/modules/user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { UserRepository } from '@modules/user/database/user.repository';
 import { UserOrmEntity } from '@modules/user/database/user.orm-entity';
 import { MailerModule } from '@nestjs-modules/mailer';
@@ -8,6 +9,7 @@ import { mailerConfig } from '@config/mailer.config';
 import { BookModule } from '@modules/book/book.module';
 import { BookRepository } from '@modules/book/database/book.repository';
 import { BookOrmEntity } from '@modules/book/database/book.orm-entity';
+import { join } from 'path';
 import * as typeormConfig from './infrastructure/configs/ormconfig';
 
 @Module({
@@ -15,6 +17,9 @@ import * as typeormConfig from './infrastructure/configs/ormconfig';
     TypeOrmModule.forRoot(typeormConfig),
     TypeOrmModule.forFeature([UserOrmEntity, BookOrmEntity]),
     MailerModule.forRoot(mailerConfig),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'upload'),
+    }),
     UserModule,
     BookModule,
   ],
--- src/infrastructure/services/image-upload.service.ts
+++ src/infrastructure/services/image-upload.service.ts
@@ -2,9 +2,17 @@ import { FileUplaodService } from '@modules/book/domain-services/file-upload.ser
 import * as fs from 'fs';
 import { imageStorageConfig } from '@config/storage.config';
 import { BookImage } from '@modules/book/domain/value-objects/image.value-object';
+import { ConflictException } from '@exceptions';
 
 export class ImageUploadService implements FileUplaodService {
   upload(file: Buffer, image: BookImage): void {
-    fs.writeFileSync(`${imageStorageConfig.dist}/${image.imageName}`, file);
+    // check if file exists
+    const filePath = `${__dirname}/../../upload/image/${image.imageName}`;
+    const exists = fs.existsSync(filePath);
+    if (exists) {
+      throw new ConflictException('File already exists');
+    } else {
+      fs.writeFileSync(`${imageStorageConfig.dist}/${image.imageName}`, file);
+    }
   }
 }
